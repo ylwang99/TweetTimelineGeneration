41c09f57e8a120bb074d8f1db02baa228e182df7
ylwang99 <wg3999@gmail.com>
2014-06-16 13:02:00 -0400

    clusters into one json file

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ This repo contains notes and working examples for the Tweet Timeline Generation 
 + [Visualization of semantic clusters](http://ylwang99.github.io/TweetTimelineGeneration/semantic-clusters.html)
 + [Visualization of semantic clusters in Twitter style](http://ylwang99.github.io/TweetTimelineGeneration/semantic-clusters-twitterstyle.html)
 + [Visualization of timelines](http://ylwang99.github.io/TweetTimelineGeneration/timeline.html)
-+ [Annotation Interface](http://ylwang99.github.io/TweetTimelineGeneration/annotation-simplified-twitterstyle.html)
++ [Annotation Interface](http://ylwang99.github.io/TweetTimelineGeneration/annotation-twitterstyle.html)
 + [Ground truth](http://ylwang99.github.io/TweetTimelineGeneration/ground-truth.html)
--- scripts/helper-generate-cluster-epoch.py
+++ scripts/helper-generate-cluster-epoch.py
@@ -4,37 +4,38 @@
 from sets import Set
 import os
 import sys
+import json
 
-path_data = "../data/timeline-representatives/"
+path_clusters = "../data/semantic-clusters-ids/json/clusters-ids.json"
+topicNums = ["03", "21", "22", "26", "42", "51", "57", "66", "68", "88"]
 path_epoch = sys.argv[1]
+representatives_all = {}
 
 if os.path.exists(path_epoch):
     os.remove(path_epoch)
 
-dirs = os.listdir(path_data)
+file_clusters = open(path_clusters, "r")
+data = json.load(file_clusters)
+for i in range(len(topicNums)):
+    representatives = Set()
+    clusters = data["topics"]["MB" + topicNums[i]]["clusters"]
+    for j in range(len(clusters)):
+        clusters[j] = [s.encode("utf-8") for s in clusters[j]]
+        representatives.add(clusters[j][0])
+    representatives_all[topicNums[i]] = representatives
+    
+
 file_epoch = open(path_epoch, "a")
-for file in dirs:
-    if file[-4:] == ".txt":
-        file_data = open(path_data + file, "r")
-        representatives = Set()
-        lines = file_data.readlines()
-        for line in lines:
-            line = line.strip().split()
-            representatives.add(line[0])
-        file_data.close()
-        file_qrels = open("../data/qrels-microblog-epoch.txt", "r")
-        lines = file_qrels.readlines()
-        for line in lines:
-            line = line.strip().split()
-            topicNum = int(file[:file.index(".")])
-            if topicNum < 100:
-                topic = "MB%02d" % topicNum
+
+for topicNum in representatives_all:
+    file_qrels = open("../data/qrels-microblog-epoch.txt", "r")
+    lines = file_qrels.readlines()
+    for line in lines:
+        line = line.strip().split()
+        if line[0] == "MB" + topicNum and int(line[3]) > 0:
+            if line[2] in representatives_all[topicNum]:
+                file_epoch.write(line[0] + " " + line[1] + " " + line[2] + " 2 " + line[4] + " " + line[5] + "\n")
             else:
-                topic = "MB%d" % topicNum 
-            if line[0] == topic and int(line[3]) > 0:
-                if line[2] in representatives:
-                    file_epoch.write(line[0] + " " + line[1] + " " + line[2] + " 2 " + line[4] + " " + line[5] + "\n")
-                else:
-                    file_epoch.write(line[0] + " " + line[1] + " " + line[2] + " 1 " + line[4] + " " + line[5] + "\n")
-        file_qrels.close()
+                file_epoch.write(line[0] + " " + line[1] + " " + line[2] + " 1 " + line[4] + " " + line[5] + "\n")
+file_qrels.close()
 file_epoch.close()
